f8f656b77232d72e200cad60aa0ca3fe3da01ef7
Thomjunior <Thompsoneno1@gmail.com>
2023-08-23 00:43:39 +0100

    modify the Betcoin-Ticker

--- index.js
+++ index.js
@@ -14,17 +14,33 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
     var crypto = req.body.crypto;
     var fiat = req.body.fiat;
-    var baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-    finalUrl = baseUrl + crypto + fiat;
-    request(finalUrl,function(error,response,body){
+    var amount = req.body.amount;
+    // var baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+    // finalUrl = baseUrl + crypto + fiat;
+
+    var options = {
+        url : "https://apiv2.bitcoinaverage.com/indices/global",
+        method : "GET",
+        qs :{
+            from:crypto,
+            to:fiat,
+            amount:amount
+        }
+
+
+
+    };
+    request(options,function(error,response,body){
         
     var data = JSON.parse(body);
-    var price = data.last;
-    var currentDate = data.display_timestamp;
+    var price = data.price;
+    console.log(price);
+    var currentDate = data.time;
     res.write("<p/>the current date is" + currentDate + " <p/>");
-    res.write("<h1> the current price of bitcoin is " + crypto + "is" + price + fiat + " <h1/>")
+    res.write("<h1>"  +  amount +crypto + "is currently worth" + price + fiat + "<h1/>")
     res.send();
     });
+
    
    
 })
